74a2b1c1d204e4eadf0b6a3d51fea0256066b0a9
Prem Nair <prem.q.nair@gmail.com>
2023-06-28 01:08:22 -0700

    Project import generated by Copybara. (#42)
    GitOrigin-RevId: 0254f337da11c591aebe10a3fe88ce8876b29be5
    
    Co-authored-by: Copybara Bot <copybara@exafunction.com>

--- codeium.el
+++ codeium.el
@@ -39,7 +39,7 @@
 
 ;;; Code:
 
-(defvar codeium-latest-local-server-version "1.2.43")
+(defvar codeium-latest-local-server-version "1.2.44")
 
 ;; (require 'url-parse)
 (autoload 'url-parse-make-urlobj "url-parse")
