02f9382c925633a19dc928e99b868fd5f6947e58
Rahul Sridhar <sridhar.rahul@gmail.com>
2023-12-21 16:41:45 -0800

    Project import generated by Copybara. (#88)
    GitOrigin-RevId: 41bc3eec7d08957d90c8977b588ec77393ee17b7
    
    Co-authored-by: Copybara Bot <copybara@exafunction.com>

--- codeium.el
+++ codeium.el
@@ -39,7 +39,7 @@
 
 ;;; Code:
 
-(defvar codeium-latest-local-server-version "1.6.5")
+(defvar codeium-latest-local-server-version "1.6.13")
 
 ;; (require 'url-parse)
 (autoload 'url-parse-make-urlobj "url-parse")
