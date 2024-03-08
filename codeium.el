49b565935ba60288594c6ed3239edc19272a32ef
Rahul Sridhar <rahul@exafunction.com>
2024-03-07 19:17:01 -0800

    Bring to head of internal repo (#94)
    * Project import generated by Copybara.
    
    GitOrigin-RevId: cf8d039bf8b9335c3a92d0395ee3003cc4dcc83d
    
    * Update codeium.el
    
    ---------
    
    Co-authored-by: Copybara Bot <copybara@exafunction.com>

--- codeium.el
+++ codeium.el
@@ -39,7 +39,7 @@
 
 ;;; Code:
 
-(defvar codeium-latest-local-server-version "1.6.28")
+(defvar codeium-latest-local-server-version "1.6.39")
 
 ;; (require 'url-parse)
 (autoload 'url-parse-make-urlobj "url-parse")
