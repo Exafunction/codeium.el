730f1c69656efdbea626db58806e83092e095935
Rahul Sridhar <sridhar.rahul@gmail.com>
2024-03-07 19:16:49 -0800

    Update codeium.el

--- codeium.el
+++ codeium.el
@@ -39,7 +39,7 @@
 
 ;;; Code:
 
-(defvar codeium-latest-local-server-version "1.8.5")
+(defvar codeium-latest-local-server-version "1.6.39")
 
 ;; (require 'url-parse)
 (autoload 'url-parse-make-urlobj "url-parse")
