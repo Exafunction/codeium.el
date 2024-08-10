08d5ecfa74d960cf18af46c2d7fa0449d789d73b
kevinzlu <100161996+kevinzlu@users.noreply.github.com>
2024-08-09 17:03:38 -0700

    Project import generated by Copybara. (#113)
    GitOrigin-RevId: 83822a4b73777af9a9273c14d369e59299c02537
    
    Co-authored-by: Copybara Bot <copybara@exafunction.com>

--- codeium.el
+++ codeium.el
@@ -39,7 +39,7 @@
 
 ;;; Code:
 
-(defvar codeium-latest-local-server-version "1.10.0")
+(defvar codeium-latest-local-server-version "1.12.0")
 
 ;; (require 'url-parse)
 (autoload 'url-parse-make-urlobj "url-parse")
